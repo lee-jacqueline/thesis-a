df9a78807b40fd526dd4b88098947c3a3afffce2
Jacqueline <leejjjacq@gmail.com>
2017-08-23 04:23:17 +1000

    Filled questionaire with information

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Input, Header, Segment, Message } from 'semantic-ui-react'
+import { Input, Header, Segment, Message, Icon } from 'semantic-ui-react'
 import { Button, Checkbox, Form, Radio, Select, TextArea } from 'semantic-ui-react'
 
 class App extends Component {
   render() {
     return (
-      <center><div>
+      <center><div className="App-background">
         <HeaderCristal />
         <TitleBar />
       </div></center>
@@ -20,7 +20,14 @@ class TitleBar extends Component {
     return (
       <div className="App">
         <MessageNotice />
-        <SegmentOne />
+        <Form>
+          <FormOne />
+          <FormTwo />
+          <FormThree />
+          <FormFour />
+          <FormFive />
+          <Button type='submit'>Submit</Button>
+        </Form>
       </div>
     );
   }
@@ -45,47 +52,205 @@ const MessageNotice = () => (
   </Message>
 )
 
-const SegmentOne = () => (
-  <Segment.Group className="Segments">
-    <Segment>
-      <FormOne />
-    </Segment>
-  </Segment.Group>
-)
-
-const options = [
-  { key: 'm', text: 'Male', value: 'male' },
-  { key: 'f', text: 'Female', value: 'female' },
-]
-
 class FormOne extends Component {
   state = {}
 
-  handleChange = (e, { value }) => this.setState({ value })
+  genderChange = (e, { value }) => this.setState({ value })
 
   render() {
     const { value } = this.state
     return (
-      <Form>
-        <Form.Group widths='equal'>
-          <Form.Field control={Input} label='First name' placeholder='First name' />
-          <Form.Field control={Input} label='Last name' placeholder='Last name' />
-          <Form.Field control={Select} label='Gender' options={options} placeholder='Gender' />
-        </Form.Group>
-        <Form.Group inline>
-          <label>Quantity</label>
-          <Form.Field control={Radio} label='One' value='1' checked={value === '1'} onChange={this.handleChange} />
-          <Form.Field control={Radio} label='Two' value='2' checked={value === '2'} onChange={this.handleChange} />
-          <Form.Field control={Radio} label='Three' value='3' checked={value === '3'} onChange={this.handleChange} />
-        </Form.Group>
-        <Form.Field control={TextArea} label='About' placeholder='Tell us more about you...' />
-        <Form.Field control={Checkbox} label='I agree to the Terms and Conditions' />
-        <Form.Field control={Button}>Submit</Form.Field>
-      </Form>
+      <Segment.Group className="Segments">
+        <Segment>
+          <Form.Group widths='equal'>
+            <Form.Field control={Input} label='Completion Date' placeholder='Date' />
+            <Form.Field control={Input} label='Study ID' placeholder='Study ID' />
+            <Form.Field control={Input} label='Date of Presentation/Hospitalisation' placeholder='Date' />
+          </Form.Group>
+          <Form.Field control={Input} label='Admitting Team' placeholder='Team' />
+        </Segment>
+      </Segment.Group>
     )
   }
 }
 
+class FormTwo extends Component {
+  state = {}
+
+  admissionChange = (e, { value }) => this.setState({ value })
+
+  render() {
+    const { value } = this.state
+    return (
+      <Segment.Group className="Segments">
+        <Segment>
+          <Gender />
+          <Form.Field control={Input} label='Date of Birth' placeholder='Date'/>
+          <Form.Field control={Checkbox} label='Age >80' />
+          <p className="Subtitles">Choose one</p>
+          <Form.Field control={Radio} label='Eligible for admission via emergency Department or at least 1 night in the ED' value='1' checked={value === '1'} onChange={this.admissionChange} />
+          <Form.Field control={Radio} label='Nursing home resident /or in supported accomodation'  value='2' checked={value === '2'} onChange={this.admissionChange} />
+          <Form.Group>
+            <Form.Field control={Input} label='ROCKWOOD Scale:' placeholder='' width={8}/>
+            <Rockwood />
+            <Form.Field control={Input} label='Actual CFS score:' placeholder='' width={3}/>
+          </Form.Group>
+        </Segment>
+      </Segment.Group>
+    )
+  }
+}
+
+class Gender extends Component {
+  state = {}
+  genderChange = (e, { value }) => this.setState({ value })
+  render() {
+    const { value } = this.state
+    return (
+      <Form.Group inline>
+        <label>Sex</label>
+        <Form.Field control={Radio} label='Male' value='1' checked={value === '1'} onChange={this.genderChange} />
+        <Form.Field control={Radio} label='Female' value='2' checked={value === '2'} onChange={this.genderChange} />
+      </Form.Group>
+    )
+  }
+}
+
+class FormThree extends Component {
+  state = {}
+
+  render() {
+    const { value } = this.state
+    return (
+      <Segment.Group className="Segments">
+        <Segment>
+
+            <strong><label>Evidence of Cognitive Impairment</label></strong>
+            <Form.Field control={Checkbox} label='Dementia' />
+            <Form.Field control={Checkbox} label='Long term mental disorder' />
+            <Form.Field control={Checkbox} label='Behavioural Alterations' />
+            <Form.Field control={Checkbox} label='Mental disability from stroke' />
+            <Proteinuria />
+            <strong><label>Abnormal ECG (atrial fibrillation, ventricular tachycardia, other abnormal rhythm or >5 ectopics/min, changes to Q or ST waves)</label></strong>
+            <Form.Group inline>
+              <Form.Field control={Checkbox} label='Acute abnormality' />
+              <Form.Field control={Checkbox} label='Chronic abnormality' />
+              <Form.Field control={Checkbox} label='Both chronic and acute' />
+            </Form.Group>
+            <Form.Field control={Input} label='Type of abnormality (optional)' placeholder=''/>
+            <Form.Group inline>
+              <Form.Field control={Checkbox} label='No abnormality' />
+              <Form.Field control={Checkbox} label='Don&#39;t Know' />
+            </Form.Group>
+        </Segment>
+      </Segment.Group>
+    )
+  }
+}
+
+class FormFour extends Component {
+  state = {}
+
+  render() {
+    const { value } = this.state
+    return (
+      <Segment.Group className="Segments">
+        <Segment>
+          <PrevHosp />
+          <Hospitalisation />
+        </Segment>
+      </Segment.Group>
+    )
+  }
+}
+
+class FormFive extends Component {
+  state = {}
+  render() {
+    const { value } = this.state
+    return (
+      <Segment.Group className="Segments">
+        <Segment>
+          <Form.Field control={Checkbox} label='Fall in the past 3 months' />
+          <Form.Field control={Checkbox} label='Polypharmacy (6 or more medications) before admission' />
+        </Segment>
+      </Segment.Group>
+    )
+  }
+}
+
+class Rockwood extends Component {
+  state = {}
+  rwChange = (e, { value }) => this.setState({ value })
+  render() {
+    const { value } = this.state
+    return (
+      <Form.Group inline>
+        <label>Rockwood >= 5</label>
+        <Form.Field control={Radio} label='Yes' value='1' checked={value === '1'} onChange={this.rwChange} />
+        <Form.Field control={Radio} label='No'  value='2' checked={value === '2'} onChange={this.rwChange} />
+      </Form.Group>
+    )
+  }
+}
+
+class Proteinuria extends Component {
+  state = {}
+  proteinChange = (e, { value }) => this.setState({ value })
+  render() {
+    const { value } = this.state
+    return (
+      <Form.Group inline>
+        <label>Proteinuria on a spot urine sample: (++ or &#62;30mg albumin/g creatinine)</label>
+        <Form.Field control={Radio} label='Yes' value='1' checked={value === '1'} onChange={this.proteinChange} />
+        <Form.Field control={Radio} label='No'  value='2' checked={value === '2'} onChange={this.proteinChange} />
+        <Form.Field control={Radio} label='Don&#39;t Know'  value='3' checked={value === '3'} onChange={this.proteinChange} />
+      </Form.Group>
+    )
+  }
+}
+
+class PrevHosp extends Component {
+  state = {}
+  prevHospChange = (e, { value }) => this.setState({ value })
+  render() {
+    const { value } = this.state
+    return (
+      <div>
+        <label>Previous Hospitalisation for at least one night in past year</label>
+        <Form.Group inline>
+          <Form.Field control={Radio} label='Yes' value='1' checked={value === '1'} onChange={this.prevHospChange} />
+          <Form.Field control={Input} label='Total number of hospitalisation in the past year' placeholder=''/>
+        </Form.Group>
+        <Form.Group inline>
+          <Form.Field control={Radio} label='No'  value='2' checked={value === '2'} onChange={this.prevHospChange} />
+          <Form.Field control={Radio} label='Not Documented'  value='3' checked={value === '3'} onChange={this.prevHospChange} />
+        </Form.Group>
+      </div>
+    )
+  }
+}
+
+class Hospitalisation extends Component {
+  state = {}
+  icuChange = (e, { value }) => this.setState({ value })
+  render() {
+    const { value } = this.state
+    return (
+      <div>
+        <label>ICU admission at previous hospitalisation in the past year</label>
+        <Form.Group inline>
+          <Form.Field control={Radio} label='Yes' value='1' checked={value === '1'} onChange={this.icuChange} />
+          <Form.Field control={Input} label='Total number of hospitalisation in the past year' placeholder=''/>
+        </Form.Group>
+        <Form.Group inline>
+          <Form.Field control={Radio} label='No ICU admission at all'  value='2' checked={value === '2'} onChange={this.icuChange} />
+          <Form.Field control={Radio} label='Unknown'  value='3' checked={value === '3'} onChange={this.icuChange} />
+        </Form.Group>
+      </div>
+    )
+  }
+}
 // const InputExampleInput = () => (
 //   <Input placeholder='Search...' />
 // )
